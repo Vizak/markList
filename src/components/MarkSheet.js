0f525f1a4e2e192cb22ddf130a5ba6d7eb342419
vishnu prakash <vishnuvictorio@gmail.com>
2022-11-18 13:56:38 +0530

    Modified service file.

--- src/components/MarkSheet.js
+++ src/components/MarkSheet.js
@@ -1,4 +1,4 @@
-/* eslint-disable max-lines-per-function */
+
 import { React } from 'react';
 import data from '../services/data';
 import addFields from '../services/studentManager';
@@ -16,32 +16,36 @@ const headings = [
 	'rank',
 ];
 
+const tableHeader = (title, index) =>
+	<th key={ index } className="tableHeaderStyle">
+		{title}
+	</th>;
+
+const tableData = (student, key) =>
+	<tr key={ key }>
+		<td className="stringStyle">{student.student}</td>
+		<td className="numberStyle">{student.rollNo}</td>
+		<td className="numberStyle">{student.tamil}</td>
+		<td className="numberStyle">{student.english}</td>
+		<td className="numberStyle">{student.science}</td>
+		<td className="numberStyle">{student.maths}</td>
+		<td className="numberStyle">{student.social}</td>
+		<td className="numberStyle">{student.total}</td>
+		<td className="stringStyle">{student.result}</td>
+		<td className="numberStyle">{student.rank}</td>
+
+	</tr>;
+
 const MarkSheet = () =>
 	<div>
 		<table className="titleStyle">
 			<thead>
 				<tr>
-					{headings.map((title, index) =>
-						<th key={ index } className="tableHeaderStyle">
-							{title}
-						</th>)}
+					{headings.map(tableHeader)}
 				</tr>
 			</thead>
 			<tbody>
-				{ addFields(data).map((student, key) =>
-					<tr key={ key }>
-						<td className="stringStyle">{student.student}</td>
-						<td className="numberStyle">{student.rollNo}</td>
-						<td className="numberStyle">{student.tamil}</td>
-						<td className="numberStyle">{student.english}</td>
-						<td className="numberStyle">{student.science}</td>
-						<td className="numberStyle">{student.maths}</td>
-						<td className="numberStyle">{student.social}</td>
-						<td className="numberStyle">{student.total}</td>
-						<td className="stringStyle">{student.result}</td>
-						<td className="numberStyle">{student.rank}</td>
-
-					</tr>)}
+				{addFields(data).map(tableData)}
 			</tbody>
 		</table>
 	</div>;
