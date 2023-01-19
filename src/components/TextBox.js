8e52d8eb091a16015d4f5692c64a07cc2c86dca8
vishnu prakash <vishnuvictorio@gmail.com>
2023-01-19 19:26:16 +0530

    modified inputBox component.

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,73 +1,19 @@
-/* eslint-disable max-lines-per-function */
-import { peek } from '@laufire/utils/debug';
 import { React } from 'react';
 
 const TextBox = (context) => {
-	const { state, setState } = context;
+	const { config: { headers }, state, setState } = context;
 	const { marks } = state;
 
-	return <div>
-		<label>RollNo:</label>
-		<input
-			type="number"
-			onChange={ (event) => setState({
-				...state,
-				marks: { ...marks, rollNo: event.target.value },
-			}) }
-		/>
-		<label>Name:</label>
+	return headers.map((header, key) => <span key={ key }>
+		<label>{header}:</label>
 		<input
 			type="text"
 			onChange={ (event) => setState({
 				...state,
-				marks: { ...marks, name: event.target.value },
+				marks: { ...marks, [header]: Number(event.target.value) },
 			}) }
 		/>
-		<label>Tamil:</label>
-		<input
-			type="number"
-			onChange={ (event) => {
-				peek(event.target.value);
-				return setState({
-					...state,
-					marks: { ...marks, tamil: Number(event.target.value) },
-				});
-			} }
-		/>
-		<label>English:</label>
-		<input
-			type="number"
-			onChange={ (event) => setState({
-				...state,
-				marks: { ...marks, english: Number(event.target.value) },
-			}) }
-		/>
-		<label>Science:</label>
-		<input
-			type="number"
-			onChange={ (event) => setState({
-				...state,
-				marks: { ...marks, science: Number(event.target.value) },
-			}) }
-		/>
-		<label>Maths:</label>
-		<input
-			type="number"
-			onChange={ (event) => setState({
-				...state,
-				marks: { ...marks, maths: Number(event.target.value) },
-			}) }
-		/>
-		<label>Social:</label>
-		<input
-			type="number"
-			onChange={ (event) => setState({
-				...state,
-				marks: { ...marks, social: Number(event.target.value) },
-			}) }
-		/>
-
-	</div>;
+	</span>);
 };
 
 export default TextBox;
